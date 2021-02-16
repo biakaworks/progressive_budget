1ff4d6608f6d2b2809cd826df5fa097e9f8db4f5
Biaka Z <biaka@biakaworks.com>
2021-02-16 11:50:36 -0600

    added Atlas DB

--- models/transaction.js
+++ models/transaction.js
@@ -23,3 +23,8 @@ const transactionSchema = new Schema(
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = Transaction;
+
+
+mongodb+srv://biaka:bootcamp@cluster0.vfvhf.mongodb.net/budget?retryWrites=true&w=majority
+
+mongodb+srv://biaka:bootcamp@cluster0.vfvhf.mongodb.net/fitnesstracker?retryWrites=true&w=majority
